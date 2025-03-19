978a0209a4a261dcdef5d96510ddd9fc8d74e2de
Kajaqq <kajos200@gmail.com>
2025-03-19 21:14:41 +0100

    Merge patching options into one repo

--- patch.sh
+++ patch.sh
@@ -18,9 +18,17 @@ java -jar APKEditor.jar m -i $xapk_name -o $apk_name
 
 java -jar lspatch.jar -l 2 --manager $apk_name
 
+java -jar lspatch_embed.jar $apk_name -d -m module.apk
+
 rm -f $xapk_name $apk_name 
 
-patched_apk=$(find *patched.apk)
+patched_apk=$(find *430-lspatched.apk)
+embed_apk=$(find *426-lspatched.apk)
+
+mv $embed_apk "$file"_embedded.apk
+
+embed_apk=$(find *_embedded.apk)
 
 echo "PATCHED_APK=$patched_apk" >> "$GITHUB_ENV"
+echo "EMBED_APK=$embed_apk" >> "$GITHUB_ENV"
 echo "APK_VERSION=$apk_version" >> "$GITHUB_ENV"
