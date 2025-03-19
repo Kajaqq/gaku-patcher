4e154f72e1865781d7943fc0385526e3aa558371
Kajaqq <kajos200@gmail.com>
2025-03-19 21:31:01 +0100

    Fix version checking 2 - Electric Boogaloo

--- ver_check.sh
+++ ver_check.sh
@@ -3,7 +3,7 @@
 var=$(python ./get_gakumas_xapk.py)
 
 apk_version=${var%https*}
-LATEST_TAG=$(git for-each-ref refs/tags --sort=-refname:short --format='%(refname:short)' --count=1)
+LATEST_TAG="$(git describe --tags "$(git rev-list --tags --max-count=1)")"
 
 echo "APK_VERSION=$apk_version" >> "$GITHUB_ENV"
 
