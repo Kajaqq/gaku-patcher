b5f9572a49a38d6e90013322721a44fb3a0755b2
Kajaqq <kajos200@gmail.com>
2025-04-22 23:08:11 +0200

    Rewrite the version checking code, again.

--- get_gakumas_xapk.py
+++ get_gakumas_xapk.py
@@ -4,80 +4,54 @@ from typing import Any
 
 import requests
 
+import yaml
 
-def _get_checkin_param() -> Any | None:
-    """
-    Fetches the checkin parameter from the APKCombo checkin endpoint.
-
-    Returns:
-        str: The checkin parameter or None if an error occurs.
-    """
-    try:
-        response = requests.post('https://apkcombo.com/checkin', timeout=10)
-        response.raise_for_status()
-        return response.text.strip()
-    except requests.exceptions.RequestException as e:
-        print(f"Error fetching checkin parameter: {e}")
-        return None
-
-
-class APKComboInfo:
-    def __init__(self, apkcombo_url: str):
-        self.apkcombo_url = apkcombo_url
-        self.xapk_download_url, self.xapk_version = self._get_xapk_download_url()
+class APKInfo:
+    def __init__(self, version_url: str):
+        self.version_url = version_url
+        self.xapk_download_url = self._get_xapk_download_url()
+        self.xapk_version = self._get_xapk_version()
 
     def _get_xapk_download_url(self) -> Any | None:
-        """
-        Extracts the XAPK download URL from the APKCombo page.
-
-        Returns:
-            str: The XAPK download URL or None if not found.
-       """
-       
-        try:
-            response = requests.get(self.apkcombo_url, timeout=10)
-            response.raise_for_status()
-            html_content = response.text
-
-            # # Find the download URL location
-            # xapk_url_start = html_content.find('<div class="download-content"') 
-            # xapk_url_start = html_content.find('href=', xapk_url_start) + len('href="')
-            # while xapk_url_start != -1:
-            #     # Find the end of the url candidate
-            #     xapk_url_end = html_content.find('"', xapk_url_start)
-            #     temp_url = requests.utils.unquote(html_content[xapk_url_start:xapk_url_end])
-            #     #If it contains the game ID, that's our download link
-            #     if 'com.bandainamcoent.idolmaster_gakuen' in temp_url:
-            #         xapk_download_url = temp_url
-            #         break
-            #     xapk_url_start = html_content.find('href=', xapk_url_end)
-            # else:
-            #     raise requests.exceptions.RequestException # No XAPK found
-
-            # # Add checkin parameter to the download URL
-            # checkin_param = _get_checkin_param()
-            # if checkin_param:
-            #     xapk_download_url += '&' + checkin_param
-
-
             # The URL can be hard-coded as it seems to always point to the latest release, and dynamically updating it would probably require a web-driver
             
-            xapk_download_url = 'https://d.apkpure.com/b/XAPK/com.bandainamcoent.idolmaster_gakuen?version=latest'
-            version_text = 'Version:'
-            version_string_start = html_content.find(version_text) + len(version_text)+1
-            version_string_end = html_content.find(' - com.bandainamcoent.idolmaster_gakuen')
-            version_string = html_content[version_string_start:version_string_end]
+            download_url = 'https://d.apkpure.com/b/XAPK/com.bandainamcoent.idolmaster_gakuen?version=latest'
+            
+            return download_url
+        
 
-            return xapk_download_url, version_string
-
+    def _get_xapk_version(self) -> str | None:
+        """
+        Retrieves the XAPK version string from a YAML file hosted at `self.version_url`.
+        The YAML file is expected to contain version information for multiple platforms.
+        This method specifically extracts the version information for the Android platform.
+        Returns:
+            str | None: The XAPK version string, formatted as 'major.minor.patch', or None if an error occurred
+                          during the process (e.g., network issues, invalid YAML format, or no Android version found).
+        Raises:
+            requests.exceptions.RequestException: If there's an issue with the HTTP request (e.g., timeout, connection error).
+            IndexError: If no Android version is found in the YAML file.
+        """
+        try:
+            response = requests.get(self.version_url, timeout=10)
+            response.raise_for_status()
+            version_yaml = response.text
+            version_object = yaml.load(version_yaml, Loader=yaml.Loader)
+            version_object = next((sub for sub in version_object if sub['platformType'] == "PlatformType_Android"), None)
+            if version_object is None:
+                raise IndexError("No Android version found in YAML")
+            version_list = list(version_object.values())[1:]
+            version_string = '.'.join(str(version) for version in version_list)
+            return version_string
+        
         except requests.exceptions.RequestException as e:
-            print(f"Error fetching XAPK info: {e}")
+            print(f"Error fetching XAPK version: {e}")
             return None
-
+            
 # Example usage:
 if __name__ == "__main__":
-    apkcombo_url = "https://apkcombo.app/%E5%AD%A6%E5%9C%92%E3%82%A2%E3%82%A4%E3%83%89%E3%83%AB%E3%83%9E%E3%82%B9%E3%82%BF%E3%83%BC/com.bandainamcoent.idolmaster_gakuen/download/apk"
-    apk_info = APKComboInfo(apkcombo_url)
+    version_url = "https://raw.githubusercontent.com/vertesan/gakumasu-diff/refs/heads/main/ForceAppVersion.yaml"
+    apk_info = APKInfo(version_url)
     xapk_url = apk_info.xapk_download_url
     xapk_version = apk_info.xapk_version
 
