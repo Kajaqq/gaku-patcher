2e5b480cf847e18d8ed4692a7be62540ee58d577
Kajaqq <kajos200@gmail.com>
2025-05-15 02:38:55 +0200

    test: rework the version checking

--- ver_check.sh
+++ ver_check.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
 
-var=$(python ./get_gakumas_xapk.py)
+apk_link=$XAPK_LINK
+user_agent=$USER_AGENT
 
-apk_version=${var%https*}
+headers=$(wget -q -S -U "$user_agent" --start-pos 999999999 "$apk_link" 2>&1)
+
+xapk_name=${headers##*=}
+
+apk_version=${xapk_name%_*};apk_version=${apk_version##*_}
+
 LATEST_TAG="$(git describe --tags "$(git rev-list --tags --max-count=1)")"
 
+echo "APK_NAME=$xapk_name" >> "$GITHUB_ENV"
 echo "APK_VERSION=$apk_version" >> "$GITHUB_ENV"
 
 echo "Latest tag: $LATEST_TAG"
 echo "Latest app version: $apk_version"
 
-test $LATEST_TAG != $apk_version 
+test "$LATEST_TAG" != "$apk_version" 
+
+
+
+
+
+
