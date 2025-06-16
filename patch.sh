7f6efe4a7c6594c40819965b84662bccf34e8354
Kajaqq <kajos200@gmail.com>
2025-06-17 01:12:13 +0200

    oops

--- patch.sh
+++ patch.sh
@@ -13,16 +13,16 @@ java -jar APKEditor.jar m -i $xapk_name -o $apk_name
 
 java -jar lspatch.jar -l 2 --manager $apk_name
 
-java -jar lspatch2.jar $apk_name -d -m module.apk
+java -jar lspatch_embed.jar $apk_name -d -m module.apk
 
 rm -f $xapk_name $apk_name 
 
-# patched_apk=$(find *430-lspatched.apk)
-# embed_apk=$(find *426-lspatched.apk)
+patched_apk=$(find *430-lspatched.apk)
+embed_apk=$(find *433-lspatched.apk)
 
-# mv $embed_apk "$file"_embedded.apk
+mv $embed_apk "$file"_embedded.apk
 
-# embed_apk=$(find *_embedded.apk)
+embed_apk=$(find *_embedded.apk)
 
-# echo "PATCHED_APK=$patched_apk" >> "$GITHUB_ENV"
-# echo "EMBED_APK=$embed_apk" >> "$GITHUB_ENV"
+echo "PATCHED_APK=$patched_apk" >> "$GITHUB_ENV"
+echo "EMBED_APK=$embed_apk" >> "$GITHUB_ENV"
