2b440ecbff8ba4ab05f552ea6cbbbf13cf9260c5
Kajaqq <kajos200@gmail.com>
2026-04-14 20:17:51 +0200

    Fix CN Localify not working.
    apk name changed, oopsie

--- download_modules.sh
+++ download_modules.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # LOCALIFY_CN_LINK=$(curl -s https://api.github.com/repos/chinosk6/gakuen-imas-localify/releases | jq -r '.[0].assets[] | select(.name | endswith(".apk")) | .browser_download_url')
-LOCALIFY_CN_LINK=https://github.com/chinosk6/gakuen-imas-localify/releases/download/v3.2.0/app-release.apk
+LOCALIFY_CN_LINK=https://github.com/chinosk6/gakuen-imas-localify/releases/download/v3.2.0/Android-GakumasLocalify-app-release.apk
 LOCALIFY_CN_NAME=LocalifyCN.apk
 LOCALIFY_EN_LINK=https://gitea.tendokyu.moe/Maji/gakumas-localify-en/releases/download/latest/Localify.apk
 LOCALIFY_EN_NAME=Localify.apk
@@ -10,7 +10,7 @@ APKEEP_LINK=https://github.com/EFForg/apkeep/releases/latest/download/apkeep-x86
 APKEEP_NAME=apkeep
 
 aria2c -j16 $LOCALIFY_CN_LINK -o $LOCALIFY_CN_NAME
-aria2c -j16 $LOCALIFY_EN_LINK -o $LOCALIFY_EN_NAME
+#aria2c -j16 $LOCALIFY_EN_LINK -o $LOCALIFY_EN_NAME
 
 aria2c -j16 $APKEEP_LINK -o $APKEEP_NAME
 chmod +x $APKEEP_NAME
