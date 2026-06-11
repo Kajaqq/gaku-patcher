2f73948dedb87b718e66e8d5eb7a99ac980b4aca
Kajaqq <kajos200@gmail.com>
2026-06-11 17:17:56 +0200

    apkeep error fix

--- download_modules.sh
+++ download_modules.sh
@@ -16,10 +16,10 @@ APKEEP_NAME=apkeep
 APKEDITOR_LINK=https://github.com/REAndroid/APKEditor/releases/download/V1.4.9/APKEditor-1.4.9.jar
 APKEDITOR_NAME=APKEditor.apk
 
-aria2c -x4 "$LOCALIFY_CN_LINK" -o $LOCALIFY_CN_NAME &
-aria2c -x4 "$LOCALIFY_EN_LINK" -o $LOCALIFY_EN_NAME &
+aria2c -x4 "$LOCALIFY_CN_LINK" -o $LOCALIFY_CN_NAME
+aria2c -x4 "$LOCALIFY_EN_LINK" -o $LOCALIFY_EN_NAME
 
-aria2c -x4 "$APKEEP_LINK" -o $APKEEP_NAME &
+aria2c -x4 "$APKEEP_LINK" -o $APKEEP_NAME
 aria2c -x4 "$APKEDITOR_LINK" -o $APKEDITOR_NAME
 
 chmod +x $APKEEP_NAME
