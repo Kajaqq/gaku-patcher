0a8b4642822826ec328572fda5dc3e227edbb6c3
Kajaqq <kajos200@gmail.com>
2026-07-24 15:27:02 +0200

    Always download the latest LSPatch version

--- download_modules.sh
+++ download_modules.sh
@@ -16,13 +16,18 @@ APKEEP_NAME=apkeep
 APKEDITOR_LINK=https://github.com/REAndroid/APKEditor/releases/download/V1.4.9/APKEditor-1.4.9.jar
 APKEDITOR_NAME=APKEditor.jar
 
+LSPATCH_LINK=https://github.com/JingMatrix/LSPatch/releases/latest/download/lspatch.jar
+LSPATCH_NAME=lspatch.jar
+
 aria2c -x4 "$LOCALIFY_CN_LINK" -o $LOCALIFY_CN_NAME
 aria2c -x4 "$LOCALIFY_EN_LINK" -o $LOCALIFY_EN_NAME
 
 aria2c -x4 "$APKEEP_LINK" -o $APKEEP_NAME
 aria2c -x4 "$APKEDITOR_LINK" -o $APKEDITOR_NAME
+aria2c -x4 "$LSPATCH_LINK" -o $LSPATCH_NAME
 
 chmod +x $APKEEP_NAME
 
 echo "LOCALIFY_CN_NAME=$LOCALIFY_CN_NAME" >> "$GITHUB_ENV"
 echo "LOCALIFY_EN_NAME=$LOCALIFY_EN_NAME" >> "$GITHUB_ENV"
+
